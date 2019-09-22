6d63c458aa7c86d7e3ac6099d5f2f8b04e6397f3
swendt57 <Lthr4ever>
2019-09-21 18:58:31 -0700

    updated the cost comparison unit tests

--- tests/cost-comparisonsSpec.js
+++ tests/cost-comparisonsSpec.js
@@ -1,46 +1,95 @@
 
 describe("cost-comparisons - assembleDataSet", function() {
 
-    let costData = [
-        {"name": "San Diego Smile Dentistry", "city": "San Diego", "procedure": "adult_cleaning", "cost": 105},
-        {"name": "San Diego Smile Dentistry", "city": "San Diego", "procedure": "composite_filling", "cost": 140},
-        {"name": "San Diego Smile Dentistry", "city": "San Diego", "procedure": "extraction", "cost": 110},
-        {"name": "San Diego Smile Dentistry", "city": "San Diego", "procedure": "root_canal", "cost": 725},
-        {"name": "San Diego Smile Dentistry", "city": "San Diego", "procedure": "porcelain_crown", "cost": 850},
-        {"name": "Advanced Smiles Dentistry", "city": "Tijuana", "procedure": "adult_cleaning", "cost": 40},
-        {"name": "Advanced Smiles Dentistry", "city": "Tijuana", "procedure": "composite_filling", "cost": 40},
-        {"name": "Advanced Smiles Dentistry", "city": "Tijuana", "procedure": "extraction", "cost": 90},
-        {"name": "Advanced Smiles Dentistry", "city": "Tijuana", "procedure": "root_canal", "cost": 250},
-        {"name": "Advanced Smiles Dentistry", "city": "Tijuana", "procedure": "porcelain_crown", "cost": 450},
-        {"name": "Great Smile Dental", "city": "San Diego", "procedure": "adult_cleaning", "cost": 95},
-        {"name": "Great Smile Dental", "city": "San Diego", "procedure": "composite_filling", "cost": 120},
-        {"name": "Great Smile Dental", "city": "San Diego", "procedure": "extraction", "cost": 110},
-        {"name": "Great Smile Dental", "city": "San Diego", "procedure": "root_canal", "cost": 765},
-        {"name": "Great Smile Dental", "city": "San Diego", "procedure": "porcelain_crown", "cost": 1105},
-        {"name": "Dental Express", "city": "San Diego", "procedure": "adult_cleaning", "cost": 100},
-        {"name": "Dental Express", "city": "San Diego", "procedure": "composite_filling", "cost": 120},
-        {"name": "Dental Express", "city": "San Diego", "procedure": "extraction", "cost": 125},
-        {"name": "Dental Express", "city": "San Diego", "procedure": "root_canal", "cost": 785},
-        {"name": "Dental Express", "city": "San Diego", "procedure": "porcelain_crown", "cost": 950},
-        {"name": "Smile Builders", "city": "Tijuana", "procedure": "adult_cleaning", "cost": 45},
-        {"name": "Smile Builders", "city": "Tijuana", "procedure": "composite_filling", "cost": 75},
-        {"name": "Smile Builders", "city": "Tijuana", "procedure": "extraction", "cost": 80},
-        {"name": "Smile Builders", "city": "Tijuana", "procedure": "root_canal", "cost": 400},
-        {"name": "Smile Builders", "city": "Tijuana", "procedure": "porcelain_crown", "cost": 550},
-        {"name": "Dr Ignacio de la Vega", "city": "Tijuana", "procedure": "adult_cleaning", "cost": 40},
-        {"name": "Dr Ignacio de la Vega", "city": "Tijuana", "procedure": "composite_filling", "cost": 65},
-        {"name": "Dr Ignacio de la Vega", "city": "Tijuana", "procedure": "extraction", "cost": 85},
-        {"name": "Dr Ignacio de la Vega", "city": "Tijuana", "procedure": "root_canal", "cost": 260},
-        {"name": "Dr Ignacio de la Vega", "city": "Tijuana", "procedure": "porcelain_crown", "cost": 475}
-    ]
+    let sampleData = [
+        {"name": "San Diego Smile Dentistry", "city": "San Diego", "procedure": "Adult Cleaning", "cost": 105},
+        {"name": "San Diego Smile Dentistry", "city": "San Diego", "procedure": "Composite Filling", "cost": 140},
+        {"name": "San Diego Smile Dentistry", "city": "San Diego", "procedure": "Extraction", "cost": 110},
+        {"name": "San Diego Smile Dentistry", "city": "San Diego", "procedure": "Root Canal", "cost": 725},
+        {"name": "San Diego Smile Dentistry", "city": "San Diego", "procedure": "Porcelain Crown", "cost": 850},
+        {"name": "Advanced Smiles Dentistry", "city": "Tijuana", "procedure": "Adult Cleaning", "cost": 40},
+        {"name": "Advanced Smiles Dentistry", "city": "Tijuana", "procedure": "Composite Filling", "cost": 40},
+        {"name": "Advanced Smiles Dentistry", "city": "Tijuana", "procedure": "Extraction", "cost": 90},
+        {"name": "Advanced Smiles Dentistry", "city": "Tijuana", "procedure": "Root Canal", "cost": 250},
+        {"name": "Advanced Smiles Dentistry", "city": "Tijuana", "procedure": "Porcelain Crown", "cost": 450},
+        {"name": "Great Smile Dental", "city": "San Diego", "procedure": "Adult Cleaning", "cost": 95},
+        {"name": "Great Smile Dental", "city": "San Diego", "procedure": "Composite Filling", "cost": 120},
+        {"name": "Great Smile Dental", "city": "San Diego", "procedure": "Extraction", "cost": 110},
+        {"name": "Great Smile Dental", "city": "San Diego", "procedure": "Root Canal", "cost": 765},
+        {"name": "Great Smile Dental", "city": "San Diego", "procedure": "Porcelain Crown", "cost": 1105},
+        {"name": "Dental Express", "city": "San Diego", "procedure": "Adult Cleaning", "cost": 100},
+        {"name": "Dental Express", "city": "San Diego", "procedure": "Composite Filling", "cost": 120},
+        {"name": "Dental Express", "city": "San Diego", "procedure": "Extraction", "cost": 125},
+        {"name": "Dental Express", "city": "San Diego", "procedure": "Root Canal", "cost": 785},
+        {"name": "Dental Express", "city": "San Diego", "procedure": "Porcelain Crown", "cost": 950},
+        {"name": "Smile Builders", "city": "Tijuana", "procedure": "Adult Cleaning", "cost": 45},
+        {"name": "Smile Builders", "city": "Tijuana", "procedure": "Composite Filling", "cost": 75},
+        {"name": "Smile Builders", "city": "Tijuana", "procedure": "Extraction", "cost": 80},
+        {"name": "Smile Builders", "city": "Tijuana", "procedure": "Root Canal", "cost": 400},
+        {"name": "Smile Builders", "city": "Tijuana", "procedure": "Porcelain Crown", "cost": 550},
+        {"name": "Dr Ignacio de la Vega", "city": "Tijuana", "procedure": "Adult Cleaning", "cost": 40},
+        {"name": "Dr Ignacio de la Vega", "city": "Tijuana", "procedure": "Composite Filling", "cost": 65},
+        {"name": "Dr Ignacio de la Vega", "city": "Tijuana", "procedure": "Extraction", "cost": 85},
+        {"name": "Dr Ignacio de la Vega", "city": "Tijuana", "procedure": "Root Canal", "cost": 260},
+        {"name": "Dr Ignacio de la Vega", "city": "Tijuana", "procedure": "Porcelain Crown", "cost": 475}
+    ];
 
-    it("should return [100, 42, 127, 60, 115, 85, 968, 492, 758, 303]", function() {
-        let result = assembleDataSet(costData);
-        expect(result).toEqual([100, 42, 127, 60, 115, 85, 968, 492, 758, 303]);
+    it("should return a properly formatted JSON array", function() {
+        let result = assembleOverlayDataSet(sampleData);
+        expect(result).toEqual([{"procedure":"Adult Cleaning", "sd_average":100, "tj_average":42},
+            {"procedure":"Composite Filling", "sd_average":127, "tj_average":60},
+            {"procedure":"Extraction", "sd_average":115, "tj_average":85},
+            {"procedure":"Root Canal", "sd_average":758, "tj_average":303},
+            {"procedure":"Porcelain Crown", "sd_average":968, "tj_average":492}]);
     });
 
-    it("should not return [100, 42, 127, 60, 758, 303]", function() {
-        let result = assembleDataSet(costData);
-        expect(result).not.toEqual([100, 42, 127, 60, 758, 303]);
+    it("should return a JSON array length of 5", function() {
+        let result = assembleOverlayDataSet(sampleData);
+        expect(result.length).toBe(5);
     });
+
+    it("should return two JSON arrays with a length of 3 each", function() {
+        let result = sortDataByCity(sampleData);
+        expect(result[0].length).toBe(15);
+        expect(result[1].length).toBe(15);
+    });
+
+    it("should return two properly formatted JSON arrays", function() {
+        let result = sortDataByCity(sampleData);
+        expect(result[0]).toEqual([
+            {"name": "San Diego Smile Dentistry", "city": "San Diego", "procedure": "Adult Cleaning", "cost": 105},
+            {"name": "San Diego Smile Dentistry", "city": "San Diego", "procedure": "Composite Filling", "cost": 140},
+            {"name": "San Diego Smile Dentistry", "city": "San Diego", "procedure": "Extraction", "cost": 110},
+            {"name": "San Diego Smile Dentistry", "city": "San Diego", "procedure": "Root Canal", "cost": 725},
+            {"name": "San Diego Smile Dentistry", "city": "San Diego", "procedure": "Porcelain Crown", "cost": 850},
+            {"name": "Great Smile Dental", "city": "San Diego", "procedure": "Adult Cleaning", "cost": 95},
+            {"name": "Great Smile Dental", "city": "San Diego", "procedure": "Composite Filling", "cost": 120},
+            {"name": "Great Smile Dental", "city": "San Diego", "procedure": "Extraction", "cost": 110},
+            {"name": "Great Smile Dental", "city": "San Diego", "procedure": "Root Canal", "cost": 765},
+            {"name": "Great Smile Dental", "city": "San Diego", "procedure": "Porcelain Crown", "cost": 1105},
+            {"name": "Dental Express", "city": "San Diego", "procedure": "Adult Cleaning", "cost": 100},
+            {"name": "Dental Express", "city": "San Diego", "procedure": "Composite Filling", "cost": 120},
+            {"name": "Dental Express", "city": "San Diego", "procedure": "Extraction", "cost": 125},
+            {"name": "Dental Express", "city": "San Diego", "procedure": "Root Canal", "cost": 785},
+            {"name": "Dental Express", "city": "San Diego", "procedure": "Porcelain Crown", "cost": 950}
+        ]);
+        expect(result[1]).toEqual([
+            {"name": "Advanced Smiles Dentistry", "city": "Tijuana", "procedure": "Adult Cleaning", "cost": 40},
+            {"name": "Advanced Smiles Dentistry", "city": "Tijuana", "procedure": "Composite Filling", "cost": 40},
+            {"name": "Advanced Smiles Dentistry", "city": "Tijuana", "procedure": "Extraction", "cost": 90},
+            {"name": "Advanced Smiles Dentistry", "city": "Tijuana", "procedure": "Root Canal", "cost": 250},
+            {"name": "Advanced Smiles Dentistry", "city": "Tijuana", "procedure": "Porcelain Crown", "cost": 450},
+            {"name": "Smile Builders", "city": "Tijuana", "procedure": "Adult Cleaning", "cost": 45},
+            {"name": "Smile Builders", "city": "Tijuana", "procedure": "Composite Filling", "cost": 75},
+            {"name": "Smile Builders", "city": "Tijuana", "procedure": "Extraction", "cost": 80},
+            {"name": "Smile Builders", "city": "Tijuana", "procedure": "Root Canal", "cost": 400},
+            {"name": "Smile Builders", "city": "Tijuana", "procedure": "Porcelain Crown", "cost": 550},
+            {"name": "Dr Ignacio de la Vega", "city": "Tijuana", "procedure": "Adult Cleaning", "cost": 40},
+            {"name": "Dr Ignacio de la Vega", "city": "Tijuana", "procedure": "Composite Filling", "cost": 65},
+            {"name": "Dr Ignacio de la Vega", "city": "Tijuana", "procedure": "Extraction", "cost": 85},
+            {"name": "Dr Ignacio de la Vega", "city": "Tijuana", "procedure": "Root Canal", "cost": 260},
+            {"name": "Dr Ignacio de la Vega", "city": "Tijuana", "procedure": "Porcelain Crown", "cost": 475}
+        ])
+    });
+
 });
--- tests/mapsSpec.js
+++ tests/mapsSpec.js
@@ -12,7 +12,7 @@ describe("maps - assembleCoordinates", function() {
         expect(result).toEqual([{lat: '32.82225', lng: '-117.157913'},{lat: '32.81897', lng: '-117.183968'},{lat: '32.753737', lng: '-117.223805'}]);
     });
 
-    it("should return array of length 3", function() {
+    it("should return an array of length 3", function() {
         let result = assembleCoordinates(dentists);
         expect(result.length).toBe(3);
     });
